0866380101adb877308f4e1246dee86bcbc3d1b3
=hudson_thom@bentley.edu <hudson_thom@bentley.edu>
2021-02-06 21:55:39 -0700

    First pages with Head tag

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+export default about = () => {
+  return (
+    <div>
+      <Head>
+        <title>About</title>
+      </Head>
+      <h1>About</h1>
+    </div>
+  )
+};
